74b697e3204a462cc133d6557af4933c78c4df49
lagartaSadia <campolino.l@gmail.com>
2025-03-16 21:32:32 -0300

    Create menu base and added soundtrack

--- code/Game.py
+++ code/Game.py
@@ -1,20 +1,16 @@
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600,600))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH,WIN_HEIGHT))
 
     def run(self, ):
-        menu = Menu(self.window)
-        menu.run()
-        pass
 
-        # while True:
-        #     for event in pygame.event.get():
-        #         if event.type == pygame.QUIT:
-        #             pygame.quit()
-        #             quit()
+        while True:
+            menu = Menu(self.window)
+            menu.run()
--- code/Menu.py
+++ code/Menu.py
@@ -1,7 +1,33 @@
+import pygame.image
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from code.Const import WIN_WIDTH
+
+
 class Menu:
     def __init__(self, window):
         self.window = window
-        pass
+        self.surf = pygame.image.load('./asset/Background/2/forestbridge.png')
+        self.rect = self.surf.get_rect()
 
-    def run(self):
-        pass
+    def run(self,):
+        pygame.mixer.music.load('./asset/Sounds/soundtrack.wav')
+        pygame.mixer.music.play(-1)
+
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(70, "Mouse Catcher", (21, 197, 27), ((WIN_WIDTH / 2), 230))
+
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
